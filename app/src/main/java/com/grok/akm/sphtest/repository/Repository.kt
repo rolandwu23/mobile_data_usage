0ed33942d9aa7d4d91c9d17008c0bf43c111a8b8
Aung Kyaw Myint <aungkyawmyint2781995@gmail.com>
2019-10-27 21:37:38 +0800

    Feature: Set up repository
    Purpose is get data from API and store offline in ROOM.
    
    When getting data from API, we don't know the total number initially.
    My approach would be getting total number by making priming API call
    and make final API call to get all data.
    
    The reason why I decided to do was total data is only 59 at current
    API response. Might need to do iterative API calls with fixed limit
    if and when API condition changes.
    
    For emitting data in offline condition, it's based on internet
    connection condition. Will need to follow NetworkBoundResource
    approach by Google Sample.

---
+++ app/src/main/java/com/grok/akm/sphtest/repository/Repository.kt
@@ -0,0 +1,69 @@
+package com.grok.akm.sphtest.repository
+
+import android.util.Log
+import androidx.annotation.WorkerThread
+import com.grok.akm.sphtest.api.ApiCallInterface
+import com.grok.akm.sphtest.db.DataDao
+import com.grok.akm.sphtest.model.MobileDataResponse
+import com.grok.akm.sphtest.util.Constants
+import io.reactivex.Observable
+import java.io.IOException
+import java.net.InetSocketAddress
+import java.net.Socket
+import javax.inject.Inject
+
+class Repository
+@Inject constructor(private val apiCallInterface: ApiCallInterface,
+                    private val dataDao: DataDao
+){
+
+    suspend fun getMobileDataUsage() : Observable<MobileDataResponse> {
+        val dataFromApi = getMobileDataUsageFromApi()
+        val dataFromDB = getMobileDataUsageFromDB()
+        return if(hasInternetConnection()){
+            Observable.concatArrayEager(dataFromApi, dataFromDB)
+        }else {
+            dataFromDB
+        }
+    }
+
+
+    fun getMobileDataUsageFromApi() : Observable<MobileDataResponse> {
+        return apiCallInterface.getMobileDataUsage(Constants.API_RESOURCE_ID, Constants.PAGE_LIMIT)
+            .flatMap {it1 ->
+
+                val total = it1.result.total
+                apiCallInterface.getMobileDataUsage(Constants.API_RESOURCE_ID,total)
+                    .doOnNext { it2 ->
+                        dataDao.setData(it2)
+                    }
+            }
+    }
+
+    fun getMobileDataUsageFromDB() : Observable<MobileDataResponse> {
+        return dataDao.getData()
+            .toObservable()
+            .doOnNext {
+                //Print log it.size :)
+                Log.e("REPOSITORY DB *** ", it.toString())
+            }
+
+    }
+
+
+    @WorkerThread
+    private suspend fun hasInternetConnection(): Boolean {
+        return try {
+            val timeoutMs = 800
+            val socket = Socket()
+            val socketAddress = InetSocketAddress("8.8.8.8", 53)
+
+            socket.connect(socketAddress, timeoutMs)
+            socket.close()
+            true
+        } catch (e: IOException) {
+            false
+        }
+    }
+
+}
